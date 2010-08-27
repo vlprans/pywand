3e08536c9a3230143f77a23f5f326c505e7e2ceb
Vlad Pranskevichus <vlad@prans.net>
2010-08-28 02:24:05 +0300

    Implement wrappers for several MagickWand entities

--- magickwand.py
+++ magickwand.py
@@ -52,7 +52,55 @@ class MagickWand(IMWrapper):
     _ctype = api.MagickWand
     _im_new = api.NewMagickWand
     _im_clone = api.CloneMagickWand
+    _im_destroy = api.DestroyMagickWand
     _im_get_exception = api.MagickGetException
+
+
+class PixelWand(IMWrapper):
+    _ctype = api.PixelWand
+    _im_new = api.NewPixelWand
+    _im_clone = api.ClonePixelWand
+    _im_destroy = api.DestroyPixelWand
+    _im_get_exception = api.PixelGetException
+
+
+class DrawingWand(IMWrapper):
+    _ctype = api.DrawingWand
+    _im_new = api.NewDrawingWand
+    _im_clone = api.CloneDrawingWand
+    _im_destroy = api.DestroyDrawingWand
+    _im_get_exception = api.DrawGetException
+
+
+class Image(IMWrapper):
+    _ctype = api.Image
+
+
+class PixelIterator(IMWrapper):
+    _ctype = api.PixelIterator
+    _im_destroy = api.DestroyPixelIterator
+    _im_clone = api.ClonePixelIterator
+    _im_get_exception = api.PixelIteratorGetException
+
+
+class Hashmap(IMWrapper):
+    _ctype = api.HashmapInfo
+    _im_destroy = api.DestroyHashmap
+
+
+class LinkedList(IMWrapper):
+    _ctype = api.LinkedListInfo
+    _im_destroy = api.DestroyLinkedList
+
+
+class SplayTree(IMWrapper):
+    _ctype = api.SplayTreeInfo
+    _im_destroy = api.DestroySplayTree
+
+
+class XMLTree(IMWrapper):
+    _ctype = api.XMLTreeInfo
+    _im_destroy = api.DestroyXMLTree
 
 
 adapt_module(api)
